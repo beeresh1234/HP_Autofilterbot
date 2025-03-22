283403ef7d695877265315256d41a546fe42a101
beeresh1234 <139480918+beeresh1234@users.noreply.github.com>
2025-03-22 16:11:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -112,7 +112,7 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 
 # Token Verification Info :
-VERIFY = bool(environ.get('VERIFY', True))
+VERIFY = bool(environ.get('VERIFY', False))
 VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'adcash.in')
 VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', 'feca054bde6705e71520bb17120194d2e89a5839')
 VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/+nxvxKoPvhCQ1YTU1')
