ece31320162c3edb4d0bc24b787eb063a794def2
beeresh1234 <139480918+beeresh1234@users.noreply.github.com>
2025-04-30 22:29:37 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,7 +37,7 @@ REQUEST_TO_JOIN_MODE = bool(environ.get('REQUEST_TO_JOIN_MODE', False)) # Set Tr
 TRY_AGAIN_BTN = bool(environ.get('TRY_AGAIN_BTN', False)) # Set True Or False (This try again button is only for request to join fsub not for normal fsub)
 
 # This Is Force Subscribe Channel, also known as Auth Channel 
-auth_channel = environ.get('AUTH_CHANNEL', '-1001889430327') # give your force subscribe channel id here else leave it blank
+auth_channel = environ.get('AUTH_CHANNEL', '-1002294661477') # give your force subscribe channel id here else leave it blank
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 
 # This Channel Is For When User Request File With command or hashtag like - /request or #request
@@ -89,7 +89,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '-1002241178189') # Pub
 
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+hf0frQmRGu41Yzll')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+g-_Sp_I-l7AzOWFl')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/KALPATARU_MOVIES')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/+ZVOs4-uSG7EzNmRl') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Master_HP_Raj')
 
